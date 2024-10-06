8659b19a898c4fa63a8590ff8b76c1ed53fbd052
leejingoo <>
2024-10-06 14:52:12 +0900

    dep 문제 2

--- Experiment.py
+++ Experiment.py
@@ -4,7 +4,9 @@ from SP_utils import create_conversational_agent, load_from_firebase, get_diag_f
 from firebase_config import get_firebase_ref
 from langchain.schema import HumanMessage, AIMessage
 import time
-from langchain.chat_models import ChatOpenAI, ChatAnthropic
+# from langchain.chat_models import ChatOpenAI, ChatAnthropic
+from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema import HumanMessage, AIMessage, SystemMessage
 from langchain.callbacks import StreamingStdOutCallbackHandler
